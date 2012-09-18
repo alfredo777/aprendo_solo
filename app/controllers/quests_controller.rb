9b17b3e436f0f849d66e9f5fe221d7de75a89c3c
Rodrigo Ayala <azul.rejected@gmail.com>
2012-09-18 13:30:12 -0500

    Controller Quest

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -1,2 +1,11 @@
 class QuestsController < ApplicationController
+  def create
+    @quest = Quest.new(params[:quest])
+
+    if @quest.save
+      redirect_to :back,:notice => "Se guardo exitosamente"
+    else
+      redirect_to :back,:notice => "No se guardo"
+    end
+  end
 end
